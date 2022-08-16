fd8f8168e85fba002c257bcc47d25eaac3a6ffc4
Marton Kovacs <narancsjuice@gmail.com>
2022-08-16 11:22:30 +0200

    Add API endpoints
    -added POST method endpoint for creating new car in db
    -added GET methods for getting by ID and getting all cars in db
    -added PUT method to update a certain car in db
    -added DELETE method to delete a certain car in db
    
    GH-3

--- app.py
+++ app.py
@@ -34,14 +34,82 @@ class Car(db.Model):
         self.description = description
         self.price = price
 
+
 # Car Schema
 class CarSchema(ma.Schema):
     class Meta:
         fields = ('id', 'manufacturer', 'type', 'year', 'description', 'price')
 
+
 # Initialize Schema
 car_schema = CarSchema()
 cars_schema = CarSchema(many=True)
+
+
+# Create car
+@app.route('/car', methods=['POST'])
+def add_car():
+    manufacturer = request.json['manufacturer']
+    type = request.json['type']
+    year = request.json['year']
+    description = request.json['description']
+    price = request.json['price']
+
+    new_car = Car(manufacturer, type, year, description, price)
+    db.session.add(new_car)
+    db.session.commit()
+
+    return car_schema.jsonify(new_car)
+
+
+# Get all cars
+@app.route('/cars', methods=['GET'])
+def get_cars():
+    all_cars = Car.query.all()
+    result = cars_schema.dump(all_cars)
+
+    return jsonify(result)
+
+
+# Get car by id
+@app.route('/car/<id>', methods=['GET'])
+def get_car(id):
+    car = Car.query.get(id)
+
+    return car_schema.jsonify(car)
+
+
+# Update car
+@app.route('/car/<id>', methods=['PUT'])
+def update_car(id):
+    car = Car.query.get(id)
+    manufacturer = request.json['manufacturer']
+    type = request.json['type']
+    year = request.json['year']
+    description = request.json['description']
+    price = request.json['price']
+
+    car.manufacturer = manufacturer
+    car.type = type
+    car.year = year
+    car.description = description
+    car.price = price
+
+    db.session.commit()
+
+    return car_schema.jsonify(car)
+
+
+# Delete car by id
+@app.route('/car/<id>', methods=['DELETE'])
+def delete_car(id):
+    car = Car.query.get(id)
+    db.session.delete(car)
+    db.session.commit()
+
+    return "Deleted car id [" + str(car.id) +"]"
+
+
 # Run application
 if __name__ == '__main__':
     app.run(debug=True)
